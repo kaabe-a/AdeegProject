4bd26f3492a7d4be233c78a3baf65e744b166a66
mahamud abdi <mahamud0660@gmil.com>
2024-04-06 10:16:50 +0300

    making components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { UilPhone } from '@iconscout/react-unicons'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Adeeg Grocery <UilPhone/></h1>
+      <h1>Adeeg Grocery</h1>
+      <Home/>
+      
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import TopNavbar from '../components/TopNavbar'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import Testimonial from '../components/Testimonial'
+import ProductList from '../components/ProductList'
+import Featured from '../components/Featured'
+import WhyUs from '../components/WhyUs'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <TopNavbar/>
+      <Navbar/>
+      <Hero/>
+      <Featured/>
+      <ProductList/>
+      <WhyUs/>
+      <Testimonial/>
+      <Footer/>
+    </div>
   )
 }
 

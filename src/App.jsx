b2ade6c61841af3cfebb0b75a6f67857f6bead25
mahamud abdi <mahamud0660@gmil.com>
2024-04-06 09:56:21 +0300

    Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { UilPhone } from '@iconscout/react-unicons'
 
 function App() {
 
   return (
     <>
-      <h1>Adeeg Grocery</h1>
+      <h1>Adeeg Grocery <UilPhone/></h1>
     </>
   )
 }

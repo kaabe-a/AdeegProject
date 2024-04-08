e8c884cd50b35ede1a470d06a419763b51f7c0cc
mahamud abdi <mahamud0660@gmil.com>
2024-04-08 11:01:44 +0300

    Adding Product List

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import Card from './Card'
+import vegetables from './product'
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <div className="container-lg pt-5">
+      <h3 class="text-center py-5">Our Products</h3>
+    <div className='row row-cols-1 g-4 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4'>
+      {vegetables.map((vegetable)=> (
+         <Card image_link={vegetable.image_link}
+                name={vegetable.name}
+                description={vegetable.description}
+                price_per_kg={vegetable.price_per_kg}
+                category={vegetable.category}
+         />
+      ))}
+    </div>
+    </div>
   )
 }
 
---
+++ src/components/product.js
@@ -0,0 +1,103 @@
+const vegetables = [
+  {
+    name: "Tomatoes",
+
+    category: "Vegetable",
+    price_per_kg: 2.0,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 80,
+    image_link: "./images/potato.png",
+  },
+  {
+    name: "watermelon",
+    category: "Vegetable",
+    price_per_kg: 1.5,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 100,
+    image_link: "./images/watermelon.png",
+  },
+  {
+    name: "Tomatoes",
+
+    category: "Vegetable",
+    price_per_kg: 2.0,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 80,
+    image_link: "./images/potato.png",
+  },
+  {
+    name: "Tomatoes",
+
+    category: "Vegetable",
+    price_per_kg: 2.0,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 80,
+    image_link: "./images/potato.png",
+  },
+  {
+    name: "carrot",
+
+    category: "Vegetable",
+    price_per_kg: 1.8,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty.” 🌽🥦🍅",
+    quantity_available_kg: 120,
+    image_link: "./images/carrot.png",
+  },
+  {
+    name: "Onion",
+
+    category: "Vegetable",
+    price_per_kg: 1.2,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 150,
+    image_link: "./images/onion.png",
+  },
+  {
+    name: "Broccoli",
+
+    category: "Vegetable",
+    price_per_kg: 2.5,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 90,
+    image_link: "./images/cabbaj.png",
+  },
+  {
+    name: "Onion",
+
+    category: "Vegetable",
+    price_per_kg: 1.2,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 150,
+    image_link: "./images/onion.png",
+  },
+  {
+    name: "Tomatoes",
+
+    category: "Vegetable",
+    price_per_kg: 2.0,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 80,
+    image_link: "./images/potato.png",
+  },
+  {
+    name: "Onion",
+
+    category: "Vegetable",
+    price_per_kg: 1.2,
+    description:
+      "In the bustling vegetable market, colors and aromas weave a vibrant tapestry of nature’s bounty. 🌽🥦🍅",
+    quantity_available_kg: 150,
+    image_link: "./images/onion.png",
+  },
+];
+
+export default vegetables;
